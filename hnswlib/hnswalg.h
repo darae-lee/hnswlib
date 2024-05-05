6db62c93f0e086e68e6cdf5773e65471fa253112
darae-lee <2darae@kaist.ac.kr>
2024-05-06 00:26:05 +0900

    .

--- hnswlib/hnswalg.h
+++ hnswlib/hnswalg.h
@@ -1093,9 +1093,9 @@ class HierarchicalNSW : public AlgorithmInterface<dist_t> {
 
         // from repairConnectionsForUpdate
         tableint currObj = entryPointCopy;
-        if (dataPointLevel < maxLevel) {
+        if (elemLevel < maxLevel) {
             dist_t curdist = fstdistfunc_(dataPoint, getDataByInternalId(currObj), dist_func_param_);
-            for (int level = maxLevel; level > dataPointLevel; level--) {
+            for (int level = maxLevel; level > elemLevel; level--) {
                 bool changed = true;
                 while (changed) {
                     changed = false;
