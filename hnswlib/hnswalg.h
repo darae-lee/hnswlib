4d90389ab509da2eaaf2b64f0e368ffde43d4a09
darae-lee <2darae@kaist.ac.kr>
2024-05-07 02:14:03 +0900

    alpha 1.1

--- hnswlib/hnswalg.h
+++ hnswlib/hnswalg.h
@@ -447,7 +447,7 @@ class HierarchicalNSW : public AlgorithmInterface<dist_t> {
             return;
         }
 
-        float alpha = 1.2;
+        float alpha = 1.1;
         std::priority_queue<std::pair<dist_t, tableint>> queue_closest;
         std::vector<std::pair<dist_t, tableint>> return_list;
         while (top_candidates.size() > 0) {
